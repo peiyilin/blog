e2f172adc17cacebd288cea12068a7a3eb4a890c
peiyilin <30493949+peiyilin@users.noreply.github.com>
2018-08-01 16:52:59 +0800

    Create BaseTestService.java

---
+++ blog-provider/src/test/java/com/pyl/blog/service/BaseTestService.java
@@ -0,0 +1,15 @@
+package com.pyl.blog.service;
+
+import com.pyl.blog.BlogProviderApplication;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author peiyilin-ds
+ * @date 2018/8/1 15:29
+ **/
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = {BlogProviderApplication.class})
+public class BaseTestService {
+}
