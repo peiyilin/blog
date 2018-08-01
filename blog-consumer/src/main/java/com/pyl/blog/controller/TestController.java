77c29b5a1f903f1f75c406018e59d0082e52f399
peiyilin <30493949+peiyilin@users.noreply.github.com>
2018-08-01 16:30:08 +0800

    Update TestController.java

--- blog-consumer/src/main/java/com/pyl/blog/controller/TestController.java
+++ blog-consumer/src/main/java/com/pyl/blog/controller/TestController.java
@@ -1,12 +1,17 @@
 package com.pyl.blog.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
-import com.pyl.blog.api.ITestService;
+import com.pyl.blog.entity.User;
+import com.pyl.blog.service.ITestService;
+import com.pyl.blog.service.IUserService;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 /**
- * @author peiyilin
+ * @author peiyilin-ds
  * @date 2018/7/27 17:55
  **/
 @RestController
@@ -15,8 +20,16 @@ public class TestController {
     @Reference(version = "1.0.0")
     private ITestService testService;
 
+    @Reference(version = "1.0.0")
+    private IUserService userService;
+
     @RequestMapping("/test")
     public String test(String msg){
         return testService.sayHello(msg);
+    }
+
+    @RequestMapping("/users/{pageIndex}/{pageSize}")
+    public List<User> getUser(@PathVariable int pageIndex,@PathVariable int pageSize){
+       return userService.getAll(pageIndex,pageSize);
     }
 }
