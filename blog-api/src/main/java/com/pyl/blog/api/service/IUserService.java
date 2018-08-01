3f5c3d8d996a654ec022d298c1c918b5f35ce2a5
peiyilin <30493949+peiyilin@users.noreply.github.com>
2018-08-01 16:23:45 +0800

    Create IUserService.java

---
+++ blog-api/src/main/java/com/pyl/blog/api/service/IUserService.java
@@ -0,0 +1,13 @@
+package com.pyl.blog.service;
+
+import com.pyl.blog.entity.User;
+
+import java.util.List;
+
+/**
+ * @author peiyilin-ds
+ * @date 2018/8/1 14:09
+ **/
+public interface IUserService {
+    List<User> getAll(int pageIndex,int pageSize);
+}
