e8e73a457f43e3fe342afcf5dd2bb6e12cb8e723
peiyilin <30493949+peiyilin@users.noreply.github.com>
2018-08-01 16:53:33 +0800

    Create UserServiceImpl.java

---
+++ blog-provider/src/test/java/com/pyl/blog/service/UserServiceImpl.java
@@ -0,0 +1,28 @@
+package com.pyl.blog.service;
+
+import com.github.pagehelper.PageHelper;
+import com.pyl.blog.dao.UserMapper;
+import com.pyl.blog.entity.User;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author peiyilin-ds
+ * @date 2018/8/1 15:28
+ **/
+public class UserSeviceImplTest extends BaseTestService{
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Test
+    public void getAll() {
+        PageHelper.startPage(1,2);
+        List<User> users = userMapper.selectAll();
+        users.forEach(user -> System.out.println("姓名："+user.getUserName()+"|年龄："+user.getUserAge()));
+    }
+}
