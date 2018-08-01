613dba9c9f874e44cecab8f8843906d0e54f0643
peiyilin <30493949+peiyilin@users.noreply.github.com>
2018-08-01 16:55:18 +0800

    Update TestServiceImplTest.java

--- blog-provider/src/test/java/com/pyl/blog/service/TestServiceImplTest.java
+++ blog-provider/src/test/java/com/pyl/blog/service/TestServiceImplTest.java
@@ -1,11 +1,7 @@
 package com.pyl.blog.service;
 
-import com.pyl.blog.BlogProviderApplication;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * @author peiyilin-ds
